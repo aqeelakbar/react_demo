0207d7b687326cf9ae8cfa60b32cd3e7b1e5dba7
Aqeel Akbar <aqeel@locutus.local>
2018-01-08 05:03:34 +0000

    react repo

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,15 +10,70 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import { API } from './constants';
+import Ships from './ships';
+import Overlay from './overlay';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  constructor() {
+    super()
+    this.state = {
+      shippingData: [],
+      width: window.innerWidth,
+      display: false
+    }
+  }
+
+  getShippingData(url) {
+    fetch(url)
+    .then(results => {
+      return results.json();
+    }).then(data => {
+      this.setState({shippingData: data.ships});
+    }).catch(error => {
+      console.log(error);
+    })
+  }
+
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  handleClick(ship, e) {
+    this.setState({
+      ship:ship,
+      display: true
+    });
+  }
+
+  closeOverlay() {
+    this.setState({
+      ship:[],
+      display: false
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  handleWindowSizeChange = () => {
+    this.setState({width: window.innerWidth});
+  }
+
+  componentDidMount() {
+    this.getShippingData(API);
+  }
+
+
   render() {
+    const {width} = this.state;
+    const platform = width <= 500 ? "mobile" : "desktop" ;
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div>
+        <Ships ships={this.state.shippingData} platform={platform} display={this.state.display} handleClick={this.handleClick.bind(this)} ship={this.state.ship} />
+        {this.state.display && <Overlay display={this.state.display} ship={this.state.ship} handleClick={this.closeOverlay.bind(this)}></Overlay>}
+      </div>
     );
   }
 }
